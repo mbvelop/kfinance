9776bf67d9a07579e59746ad511452cd2104160e
breucode <pbreuer@breuco.de>
2020-10-22 00:18:58 +0200

    Add tests and increase double comparison accuracy

--- src/test/kotlin/com/mbvelop/kfinance/KFinanceKtTest.kt
+++ src/test/kotlin/com/mbvelop/kfinance/KFinanceKtTest.kt
@@ -3,30 +3,82 @@ package com.mbvelop.kfinance
 import com.mbvelop.kfinance.enums.PaymentSchedule.BEGIN
 import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import kotlin.math.pow
 
 class KFinanceKtTest {
 
-    @Test
-    fun testTotalPayment() {
-        val result = totalPayment(0.08 / 12, 5 * 12.0, 15000.0)
-        result shouldBe (-304.145914 plusOrMinus 0.001)
+    @Nested
+    inner class TotalPayment {
+        @Test
+        fun totalPayment() {
+            val result = totalPayment(0.08 / 12, 5 * 12.0, 15000.0)
+            result shouldBe (-304.1459143262052370338701494 plusOrMinus 10.0.pow(-10))
+        }
+
+        @Test
+        fun totalPaymentRate0() {
+            val result = totalPayment(0.0, 5 * 12.0, 15000.0)
+            result shouldBe (-250.0 plusOrMinus 10.0.pow(-10))
+        }
+
+        @Test
+        fun totalPaymentPaymentScheduleBegin() {
+            val result = totalPayment(
+                0.08 / 12,
+                5 * 12.0,
+                15000.0,
+                paymentSchedule = BEGIN
+            )
+            result shouldBe (-302.1317029 plusOrMinus 10.0.pow(-7))
+        }
+
+        @Test
+        fun totalPaymentAdditionalInputValues() {
+            var result = totalPayment(0.0, 12.0, 2000.0)
+            result shouldBe (-166.6666666666666666666666667 plusOrMinus 10.0.pow(-10))
+
+            result = totalPayment(0.3, 12.0, 2000.0)
+            result shouldBe (-626.90814017007577484025866 plusOrMinus 10.0.pow(-10))
+
+            result = totalPayment(0.8, 3.0, 20000.0)
+            result shouldBe (-19311.25827814569536423841060 plusOrMinus 10.0.pow(-10))
+        }
     }
 
-    @Test
-    fun testPmtRate0() {
-        val result = totalPayment(0.0, 5 * 12.0, 15000.0)
-        result shouldBe (-250.0 plusOrMinus 0.001)
-    }
+    @Nested
+    inner class FutureValue {
+        @Test
+        fun futureValue() {
+            val result = futureValue(0.075, 20.0, -2000.0)
 
-    @Test
-    fun testPmtPaymentScheduleBegin() {
-        val result = totalPayment(
-            0.08 / 12,
-            5 * 12.0,
-            15000.0,
-            paymentSchedule = BEGIN
-        )
-        result shouldBe (-302.1317029 plusOrMinus 0.001)
+            result shouldBe (86609.362673042924 plusOrMinus 10.0.pow(-10))
+        }
+
+        @Test
+        fun futureValuePaymentScheduleBegin() {
+            val result = futureValue(0.075, 20.0, -2000.0, paymentSchedule = BEGIN)
+
+            result shouldBe (93105.064874 plusOrMinus 10.0.pow(-6))
+        }
+
+        @Test
+        fun futureValueAdditionalInputValues() {
+            var result = futureValue(0.1, 5.0, 100.0)
+            result shouldBe (-610.510000 plusOrMinus 10.0.pow(-10))
+
+            result = futureValue(0.2, 5.0, 100.0)
+            result shouldBe (-744.160000 plusOrMinus 10.0.pow(-10))
+
+            result = futureValue(0.1, 5.0, 100.0, paymentSchedule = BEGIN)
+            result shouldBe (-671.561000 plusOrMinus 10.0.pow(-10))
+
+            result = futureValue(0.2, 5.0, 100.0, paymentSchedule = BEGIN)
+            result shouldBe (-892.992000 plusOrMinus 10.0.pow(-10))
+
+            result = futureValue(0.0, 5.0, 100.0)
+            result shouldBe (-500.0 plusOrMinus 10.0.pow(-10))
+        }
     }
 }
